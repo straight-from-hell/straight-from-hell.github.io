98fc43a562e14efcc8616cef652c6e84fa0441bb
straight-from-hell <123125398+straight-from-hell@users.noreply.github.com>
2026-08-11 22:48:44 -0400

    Update dictionary.js

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,4 +1,15 @@
 // get the <p> text, split at slashes, create tables
 var list = document.getElementById("list");
+var start = 0;
+var end = 0;
+var loopSize = 0;
 
-console.log((list.innerHTML.match(new RegExp("<p>", "g")) || []).length);
+// console.log((list.innerHTML.match(new RegExp("<p>", "g")) || []).length);
+start = list.innerHTML.indexOf("<p>");
+end = list.innerHTML.indexOf("<p>", start+1);
+
+for (var i = start; i < end; i++){
+  if (list.innerHTML[i] == "/"){
+    console.log(i);
+  }
+}
