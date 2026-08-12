002ab2423ca16eb7ab9d4ee94134d384789a9c4a
straight-from-hell <123125398+straight-from-hell@users.noreply.github.com>
2026-08-12 17:30:28 -0400

    Update dictionary.js

--- js/dictionary.js
+++ js/dictionary.js
@@ -8,13 +8,19 @@ var lastIndex = 0;
 // console.log((list.innerHTML.match(new RegExp("<p>", "g")) || []).length);
 
 for (var j = 0; j < loopSize; j++){
-  start = list.innerHTML.indexOf("<p>");
-  end = list.innerHTML.indexOf("</p>");
+  // start = list.innerHTML.indexOf("<p>");
+  // end = list.innerHTML.indexOf("</p>");
+
+  list.innerHTML.replace("<p>", "<table><tr><td>");
+  start = list.innerHTML.indexOf("<td>") + 4;
   
-  for (var i = start; i < end; i++){
+  for (var i = start; i < end; i++){ // while loop ?
     console.log(list[i]);
     if (list.innerHTML[i] == "/"){
       console.log(i);
+      list.innerHTML.replace("/", "</td>");
     }
   }
+
+  end = list.innerHTML.indexOf("</p>");
 }
