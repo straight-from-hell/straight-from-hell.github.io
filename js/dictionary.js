cb55e0cdf6f2042db6e07cd562a19658c2e2fd8c
straight-from-hell <123125398+straight-from-hell@users.noreply.github.com>
2026-08-13 08:53:17 -0400

    Update dictionary.js

--- js/dictionary.js
+++ js/dictionary.js
@@ -7,23 +7,23 @@ var lastIndex = 0;
 
 // console.log((list.innerHTML.match(new RegExp("<p>", "g")) || []).length);
 
-for (var j = 0; j < loopSize; j++){
-  start = list.innerHTML.indexOf("<p>");
-  // end = list.innerHTML.indexOf("</p>");
+// for (var j = 0; j < loopSize; j++){
+//   start = list.innerHTML.indexOf("<p>");
+//   // end = list.innerHTML.indexOf("</p>");
 
-  list.innerHTML = list.innerHTML.replace("<p>", "<table><td>");
-  start = list.innerHTML.indexOf("<td>") + 4;
+//   list.innerHTML = list.innerHTML.replace("<p>", "<table><td>");
+//   start = list.innerHTML.indexOf("<td>") + 4;
   
-  for (var i = start; i < end; i++){ // while loop ?
-    console.log(list.innerHTML[i]);
-    if (list.innerHTML[i] == "/"){
-      // console.log(i);
-      list.innerHTML = list.innerHTML.replace("/", "</td><td>");
-      end = list.innerHTML.indexOf("</p>"); // or just change it here :/
-    }
-  }
+//   for (var i = start; i < end; i++){ // while loop ?
+//     console.log(list.innerHTML[i]);
+//     if (list.innerHTML[i] == "/"){
+//       // console.log(i);
+//       list.innerHTML = list.innerHTML.replace("/", "</td><td>");
+//       end = list.innerHTML.indexOf("</p>"); // or just change it here :/
+//     }
+//   }
 
-  list.innerHTML = list.innerHTML.replace("</p>", "</td></table>");
+//   list.innerHTML = list.innerHTML.replace("</p>", "</td></table>");
 
-  // end = list.innerHTML.indexOf("</p>");
-}
+//   // end = list.innerHTML.indexOf("</p>");
+// }
