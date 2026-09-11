ddb8aca0d65e7bb972a25e88e26756bc338679a4
straight-from-hell <123125398+straight-from-hell@users.noreply.github.com>
2026-09-11 15:46:25 -0400

    Update entries.js

--- dictionary/entries.js
+++ dictionary/entries.js
@@ -2,7 +2,7 @@ fetch("words.json")
 .then(data => data.json())
 .then(json => {
 
-  console.log(json[3]);
+  console.log(json);
 
   // var sorted = json.sort((a,b) =>  a.numVal[0] - b.numVal[0]);
   // sorted.forEach(entry =>{
