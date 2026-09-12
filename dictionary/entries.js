396e861167c11ffb9c8ef3842e509ab9054409d6
straight-from-hell <123125398+straight-from-hell@users.noreply.github.com>
2026-09-11 21:13:13 -0400

    Update entries.js

--- dictionary/entries.js
+++ dictionary/entries.js
@@ -2,11 +2,11 @@ fetch("words.json")
 .then(data => data.json())
 .then(json => {
 
-  console.log(json.words[3]);
+  console.log(json.words);
 
-  // var sorted = json.sort((a,b) =>  a.numVal[0] - b.numVal[0]);
-  // sorted.forEach(entry =>{
-  //   console.log(entry.entry+", "+entry.speechPart.key)
+  var sorted = json.words.sort((a,b) =>  a.numVal[0] - b.numVal[0]);
+  sorted.forEach(entry =>{
+    console.log(entry.entry+", "+entry.speechPart.key)
   // ----- doesn't work :/ -----
 
   // json.forEach(entry =>{
