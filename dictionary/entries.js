591ccb5d45807aac49c1804a05f3f3f2e58f3d8e
straight-from-hell <123125398+straight-from-hell@users.noreply.github.com>
2026-09-11 23:18:21 -0400

    Update entries.js

--- dictionary/entries.js
+++ dictionary/entries.js
@@ -2,14 +2,16 @@ fetch("words.json")
 .then(data => data.json())
 .then(json => {
 
-  console.log(json.words);
+  // console.log(json.words);
 
   var sorted = json.words.sort((a,b) =>  a.numVal[0] - b.numVal[0]);
-  sorted.forEach(entry =>{
-    console.log(entry.entry+", "+entry.speechPart.key)
-  // ----- doesn't work :/ -----
-
-  // json.forEach(entry =>{
-  //   console.log(entry.entry+", "+entry.speechPart.key)
-  })
+  for (var i = 0; i < 35; i++){
+    var table = document.getElementById(String(i));
+    sorted.forEach(entry =>{
+      // console.log(entry.entry+", "+entry.speechPart.key)
+      if (entry.numVal[0] == i){
+        table.innerHTML += "<td class='entry'>"+entry.entry+"</td>";
+      }
+    })
+  }
 })
