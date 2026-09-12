a522e533d453f13c17a3202bb406b12d874b121a
straight-from-hell <123125398+straight-from-hell@users.noreply.github.com>
2026-09-12 12:49:42 -0400

    Update entries.js

--- dictionary/entries.js
+++ dictionary/entries.js
@@ -7,15 +7,28 @@ fetch("words.json")
   var sorted = json.words.sort((a,b) =>  a.numVal[0] - b.numVal[0]);
   for (var i = 0; i < 35; i++){
     var table = document.getElementById(String(i));
-    var sectionSorted;
+    var section = [];
 
-    for (var x = 0; x <= 3; x++){
-      sectionSort = sorted.sort((a,b) => a.numVal[x] - b.numVal[x]);
-    }
-    sectionSort.forEach(entry =>{ // just gotta figure out how to do the row thing
+    sorted.forEach(entry => {
       if (entry.numVal[0] == i){
-        table.innerHTML += "<td class='entry'>"+entry.entry+"</td>";
+        letterList.push(entry);
       }
-    })
+    });
+
+    section = section.sort((a,b) => a.numVal[1] - b.numVal[1]);
+
+    section.forEach(entry => {
+      table.innerHTML += "<td class='entry'>"+entry.entry+"</td>";
+    });
+    // var sectionSorted;
+
+    // for (var x = 0; x <= 3; x++){
+    //   sectionSort = sorted.sort((a,b) => a.numVal[x] - b.numVal[x]);
+    // }
+    // sectionSort.forEach(entry =>{ // just gotta figure out how to do the row thing
+    //   if (entry.numVal[0] == i){
+    //     table.innerHTML += "<td class='entry'>"+entry.entry+"</td>";
+    //   }
+    // })
   }
-})
+});
