862c922e5fa1fb2a1b99370129d1240a98635772
straight-from-hell <123125398+straight-from-hell@users.noreply.github.com>
2026-09-12 10:45:54 -0400

    Update entries.js

--- dictionary/entries.js
+++ dictionary/entries.js
@@ -7,11 +7,12 @@ fetch("words.json")
   var sorted = json.words.sort((a,b) =>  a.numVal[0] - b.numVal[0]);
   for (var i = 0; i < 35; i++){
     var table = document.getElementById(String(i));
+    var sectionSorted;
 
     for (var x = 0; x <= 3; x++){
-      var sectionSort = sorted.sort((a,b) => a.numVal[x] - b.numVal[x]);
+      sectionSort = sorted.sort((a,b) => a.numVal[x] - b.numVal[x]);
     }
-    sectionSort.forEach(entry =>{
+    sectionSort.forEach(entry =>{ // just gotta figure out how to do the row thing
       if (entry.numVal[0] == i){
         table.innerHTML += "<td class='entry'>"+entry.entry+"</td>";
       }
